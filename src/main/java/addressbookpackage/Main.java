22330f7bb0ef931e940a2c252a94caf82b86b149
akashchavhan22 <aschavhan999@gmail.com>
2023-03-25 00:18:12 +0530

    update delete added

--- src/main/java/addressbookpackage/Main.java
+++ src/main/java/addressbookpackage/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Contact c1 = new Contact("Akash","Chavhan","Amt","Amravati","Amravati",444602,878856042,
+        Contact c1 = new Contact("Akash","Chavhan","Amt","Amravati","MH",444602,878856042,
                 "abc@gmail.com");
         Contact c2 = new Contact("Virat","Kohli","Delhi","Delhi","Delhi",444602,878856042,
                 "virat@gmail.com");
@@ -12,6 +12,11 @@ public class Main {
         ArrayList<Contact> AddressBook = new ArrayList<Contact>();
         AddressBook.add(c1);
         AddressBook.add(c2);
-        System.out.println(AddressBook);
+        System.out.println("Before Update"+AddressBook);
+        AddressBook.set(0,new Contact("Rohit","Sharma","Mumbai","bandra","MH",444502,546879,"abc@gamil.com"));
+        System.out.println("After Update"+AddressBook);
+        AddressBook.remove(1);
+        System.out.println("After Delete"+AddressBook);
+
     }
 }
